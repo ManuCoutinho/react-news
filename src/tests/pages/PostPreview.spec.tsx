f231bcb30fe8de786fb8ac6c633f2022419717a9
ManuCoutinho <gdn.sasha@gmail.com>
2022-03-25 21:42:03 -0300

    version for deploy

--- src/tests/pages/PostPreview.spec.tsx
+++ src/tests/pages/PostPreview.spec.tsx
@@ -1,85 +1,85 @@
-import { render, screen } from "@testing-library/react";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
-import Post, { getStaticProps } from "../../pages/posts/preview/[slug]";
-import { getPrismicClient } from "../../services/prismic";
+import { render, screen } from '@testing-library/react'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
+import Post, { getStaticProps } from '../../pages/posts/preview/[slug]'
+import { getPrismicClient } from '../../services/prismic'
 
 const post = {
-  slug: "my-post",
-  title: "Title here",
-  content: "<p>Post content</p>",
-  updatedAt: "23 de março de 2022",
-};
+	slug: 'my-post',
+	title: 'Title here',
+	content: '<p>Post content</p>',
+	updatedAt: '23 de março de 2022',
+}
 
-jest.mock("next-auth/react");
-jest.mock("../../services/prismic");
-jest.mock("next/router");
+jest.mock('next-auth/react')
+jest.mock('../../services/prismic')
+jest.mock('next/router')
 
-describe("Post preview page", () => {
-  it("renders correctly", () => {
-    const useSessionMocked = jest.mocked(useSession);
-    useSessionMocked.mockReturnValueOnce({
-      data: null,
-      status: "unauthenticated",
-    });
+describe('Post preview page', () => {
+	it('renders correctly', () => {
+		const useSessionMocked = jest.mocked(useSession)
+		useSessionMocked.mockReturnValueOnce({
+			data: null,
+			status: 'unauthenticated',
+		})
 
-    render(<Post post={post} />);
+		render(<Post post={post} />)
 
-    expect(screen.getByText("Title here")).toBeInTheDocument();
-    expect(screen.getByText("Post content")).toBeInTheDocument();
-    expect(screen.getByText("Wanna continue reading?")).toBeInTheDocument();
-  });
+		expect(screen.getByText('Title here')).toBeInTheDocument()
+		expect(screen.getByText('Post content')).toBeInTheDocument()
+		expect(screen.getByText('Wanna continue reading?')).toBeInTheDocument()
+	})
 
-  it("redirects user to full post when is subscribed", async () => {
-    const useSessionMocked = jest.mocked(useSession);
-    const useRouterMocked = jest.mocked(useRouter);
-    const pushMock = jest.fn();
+	it('redirects user to full post when is subscribed', async () => {
+		const useSessionMocked = jest.mocked(useSession)
+		const useRouterMocked = jest.mocked(useRouter)
+		const pushMock = jest.fn()
 
-    useSessionMocked.mockReturnValueOnce([
-      {
-        data: {
-          activeSubscription: "fake-active-subscription",
-        },
-      },
-      false,
-    ] as any);
+		useSessionMocked.mockReturnValueOnce([
+			{
+				data: {
+					activeSubscription: 'fake-active-subscription',
+				},
+			},
+			false,
+		] as any)
 
-    useRouterMocked.mockReturnValueOnce({
-      push: pushMock,
-    } as any);
+		useRouterMocked.mockReturnValueOnce({
+			push: pushMock,
+		} as any)
 
-    render(<Post post={post} />);
+		render(<Post post={post} />)
 
-    expect(pushMock).toHaveBeenCalledWith("/posts/my-post");
-  });
+		expect(pushMock).toHaveBeenCalledWith('/posts/my-post')
+	})
 
-  it("loads ", async () => {
-    const getPrismicClientMocked = jest.mocked(getPrismicClient);
-    getPrismicClientMocked.mockReturnValueOnce({
-      getByUID: jest.fn().mockResolvedValueOnce({
-        data: {
-          title: [{ type: "heading", text: "Title here" }],
-          content: [{ type: "paragraph", text: "Post content" }],
-        },
-        last_publication_date: "03-23-2022",
-      }),
-    } as any);
+	it('loads correctly', async () => {
+		const getPrismicClientMocked = jest.mocked(getPrismicClient)
+		getPrismicClientMocked.mockReturnValueOnce({
+			getByUID: jest.fn().mockResolvedValueOnce({
+				data: {
+					title: [{ type: 'heading', text: 'Title here' }],
+					content: [{ type: 'paragraph', text: 'Post content' }],
+				},
+				last_publication_date: '03-23-2022',
+			}),
+		} as any)
 
-    const response = await getStaticProps({
-      params: { slug: "my-post" },
-    } as any);
+		const response = await getStaticProps({
+			params: { slug: 'my-post' },
+		} as any)
 
-    expect(response).toEqual(
-      expect.objectContaining({
-        props: {
-          post: {
-            slug: "my-post",
-            title: "Title here",
-            content: "<p>Post content</p>",
-            updatedAt: "23 de março de 2022",
-          },
-        },
-      })
-    );
-  });
-});
+		expect(response).toEqual(
+			expect.objectContaining({
+				props: {
+					post: {
+						slug: 'my-post',
+						title: 'Title here',
+						content: '<p>Post content</p>',
+						updatedAt: '23 de março de 2022',
+					},
+				},
+			})
+		)
+	})
+})
